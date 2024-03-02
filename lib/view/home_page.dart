7f2e9ba7b35138d7fe7bf9611f8a97b905b43145
rashi8643 <rashimuhammed86@gmail.com>
2024-03-02 11:51:02 +0530

    change home page ui

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -6,20 +6,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF151515),
       appBar: AppBar(
-        title: Text('Resume Builder'),
+        centerTitle: true,
+        title: Text(
+          'Resume Builder',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Color(0xFF0ab6ab),
       ),
-      body: ListView.separated(
+      body: ListView.builder(
         itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () => () {},
-            title: Text("name"),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white12),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "name",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "position",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         },
-        separatorBuilder: (context, index) => const SizedBox(height: 16),
         itemCount: 2,
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF0ab6ab),
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
